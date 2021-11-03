4ba1601a2cc4128645c631fa9eb7e2cab9b05804
Jully Kado Mercado Elias <jully_000@hotmail.com>
2021-11-03 12:11:17 -0600

    Creacion de proyecto
    Se crea el proyecto de unity y se genera el primer script Hola Mundo

---
+++ PrimerProyectoEbac/Assets/Scripts/holamundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holamundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Print Hola mundo");
+        Debug.Log("Log Hola mundo");
+        Debug.LogWarning("LogWarning Hola Mundo");
+        Debug.LogError("LogError Hola Mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
